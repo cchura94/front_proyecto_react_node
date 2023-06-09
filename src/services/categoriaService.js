38edf31e5716fcff17a58b2af5fbab9da80466e6
cchura94 <cchura.cpc@gmail.com>
2023-06-08 21:00:56 -0400

    crud categorias

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,12 +1,24 @@
 import api from "./api";
 
 const categoriaService = {
-    listar: () => {
-        return api.get("/categoria")
+    listar: (q="") => {
+        return api.get(`/categoria?q=${q}`)
     },
 
     guardar: (datos) => {
         return api.post("/categoria", datos)
+    },
+    
+    mostrar: (id) => {
+        return api.get(`/categoria/${id}`)
+    },
+
+    modificar: (id, datos) => {
+        return api.put(`/categoria/${id}`, datos)
+    },
+
+    eliminar: (id) => {
+        return api.delete(`/categoria/${id}`)
     }
 }
 
--- src/views/Categoria.jsx
+++ src/views/Categoria.jsx
@@ -4,18 +4,71 @@ import categoriaService from "../services/categoriaService";
 const Categoria = () => {
     // estado
     const [categorias, setCategorias] = useState([])
+    const [nombre, setNombre] = useState("");
+    const [detalle, setDetalle] = useState("");
+    const [idseleccion, setIdSeleccion] = useState(null);
+    const [buscar, setBuscar] = useState("");
+
     useEffect(() => {
         listar()
     }, [])
     // funciones
 
     const listar = async () => {
-        const {data} = await categoriaService.listar()
+        const {data} = await categoriaService.listar(buscar)
         setCategorias(data)
+    }
+
+    const guardar = async (e) => {
+        e.preventDefault();
+
+        if(idseleccion){
+            await categoriaService.modificar(idseleccion,{nombre, detalle})
+        }else{
+            await categoriaService.guardar({nombre, detalle})
+        }
+        listar()
+
+        setNombre("");
+        setDetalle("");
+        setIdSeleccion(null)
+    }
+
+    const editarCat = async (cat) => {
+        setNombre(cat.nombre);
+        setDetalle(cat.detalle);
+        setIdSeleccion(cat.id)
+        console.log(cat)
+
+    }
+
+    const eliminarCat = async(cat) => {
+        if(confirm("¿Está seguro de eliminar la categoria?")){
+            await categoriaService.eliminar(cat.id)
+            listar()
+        }
+    }
+
+    const funBuscar= (e) => {
+        setBuscar(e.target.value)
+        listar()
     }
 
     return (
         <div>
+
+            <form onSubmit={(e) => guardar(e)}>
+                <label htmlFor="">Ingrese Nombre:</label>
+                <input type="text" value={nombre} onChange={e => setNombre(e.target.value)} />
+                <br />
+                <label htmlFor="">Ingrese Detalle</label>
+                <textarea cols="30" rows="3" value={detalle?detalle:''} onChange={e => setDetalle(e.target.value)}></textarea>
+                <br />
+                <input type="submit" />
+            </form>
+
+            <hr />
+             BUSCAR POR NOMBRE: <input type="text" onChange={(e) => funBuscar(e)} />
             <table border="1">
                 <thead>
                     <tr>
@@ -31,11 +84,13 @@ const Categoria = () => {
                                 <td>{cat.id}</td>
                                 <td>{cat.nombre}</td>
                                 <td>{cat.detalle}</td>
-                                <td></td>
+                                <td>
+                                    <button onClick={() => editarCat(cat)}>editar</button>
+                                    <button onClick={() => eliminarCat(cat)}>eliminar</button>
+                                </td>
                             </tr>
                         )
                     )}
-
                 </tbody>
             </table>
 
