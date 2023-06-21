1fb9894b891fe98f8837b34d5ea8f8a252bbad9c
cchura94 <cchura.cpc@gmail.com>
2023-06-20 21:02:44 -0400

    crud producros

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,15 @@
-const Modal = (props) => {
+const Modal = ({children, modalOpen, setOpenModal}) => {
     return (
         <>
-            {props.modalOpen && (
+            {modalOpen && (
 
                 <div className="fixed inset-0 flex items-center justify-center z-50">
                     <div className="fixed inset-0 bg-black opacity-50"></div>
                     <div className="bg-white rounded-lg p-6 w-2/3 md:w-1/2 xl:w-1/3 relative">
                         <h2>Mi Titulo</h2>
-                        {props.children}
+                        {children}
 
-                        <button className="bg-gray-300 hover:bg-gray-400 rounded px-4 py-2 mt-4 absolute top-0 right-0 mr-4 mt-4" onClick={() => props.setOpenModal(false)}>X</button>
+                        <button className="bg-gray-300 hover:bg-gray-400 rounded px-4 py-2 mt-4 absolute top-0 right-0 mr-4 mt-4" onClick={() => setOpenModal(false)}>X</button>
                     </div>
                 </div>
 
